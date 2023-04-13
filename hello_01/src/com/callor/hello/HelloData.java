470c11927fcc75bc5c47d8a2a884150cadd176f8
2gechan <7532602@gmail.com>
2023-04-13 12:47:19 +0900

    문자열연산

---
+++ hello_01/src/com/callor/hello/HelloData.java
@@ -0,0 +1,15 @@
+package com.callor.hello;
+
+public class HelloData {
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+		System.out.println("Hello" + "Korea");
+		System.out.println("Hello      Korea");
+		System.out.println("Hello"        +        "Korea");
+		
+		System.out.println(30 + 40);
+		System.out.println("30" + "40");
+		System.out.println("30 + 40");
+	}
+
+}
