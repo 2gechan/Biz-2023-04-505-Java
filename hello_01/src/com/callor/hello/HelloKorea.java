89b2005429e7d5f9f1005c4e1a39027e67f83234
2gechan <7532602@gmail.com>
2023-04-13 10:19:42 +0900

    새로운_저장소

---
+++ hello_01/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,14 @@
+package com.callor.hello;
+
+
+public class HelloKorea {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1 + 1);
+		System.out.println(2 - 1);
+		System.out.println(3 * 2);
+		System.out.println(4 / 2);
+		
+	}
+}
