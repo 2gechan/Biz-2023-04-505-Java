9d7a575bc5de60664a9c5814bc1fd17a7761dfcc
2gechan <7532602@gmail.com>
2023-04-25 16:38:23 +0900

    Arrays

--- Java_008_Class/src/com/callor/classes/arrays/ArrayF.java
+++ Java_008_Class/src/com/callor/classes/arrays/ArrayF.java
@@ -1,32 +1,47 @@
 package com.callor.classes.arrays;
 
 public class ArrayF {
-	public static void prime(int rndNum) {
-		
+	public static int prime(int rndNum) {
+
 		int index;
-		for(index=2; index< rndNum; index++) {
-			if(rndNum % index == 0) {
-				break;
+		for (index = 2; index < rndNum; index++) {
+			if (rndNum % index == 0) {
+				return 0;
 			}
 		}
-		
-		if(rndNum <= index) {
-			System.out.println(rndNum + " 소수");
-		} else {
-			System.err.println(rndNum + " 소수 아님");
-		}
-		
+
+		return rndNum;
+
 	}
-	
+
+	/*
+	 * 다수의 데이터를 정보처리 할 경우
+	 * 데이터를 수집하고, 수집이 완료되었을 때
+	 * 한꺼번에 처리를 한다.
+	 * 
+	 * 실시간 처리 : 데이터가 발생하는 즉시 처리 ex) 티켓 예매
+	 * 배치(Batch) 처리 : 데이터를 모두 수집하고 수집이 완료되었을때 처리
+	 * 						ex) 급여계산, 인구 통계
+	 */
 	public static void main(String[] args) {
 		int[] nums = new int[10];
 
-		for(int i=0; i<nums.length; i++) {
-			nums[i] = (int)(Math.random() * 50) + 51;
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = (int) (Math.random() * 50) + 51;
 		}
 		
-		for(int i=0; i<nums.length; i++) {
-			prime(nums[i]);
+		// 데이터를 수집 완료 후 처리
+
+		for (int i = 0; i < nums.length; i++) {
+			
+			int result = prime(nums[i]); // nums[i]는 int[] 안에 있는 한개의 정수형
+			
+			if (result > 0) {
+				System.out.println(nums[i] + " 는 소수");
+			} else {
+				System.err.println(nums[i] + " 는 소수 아님");
+				
+			}
 		}
 	}
 
