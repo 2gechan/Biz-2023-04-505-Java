d42df2d445dba6170a0cd363cf2210d095baafe4
2gechan <7532602@gmail.com>
2023-05-02 20:08:25 +0900

    05/02_과제

--- Java_011_Class/src/com/callor/classes/ClassH.java
+++ Java_011_Class/src/com/callor/classes/ClassH.java
@@ -10,24 +10,28 @@ public class ClassH {
 
 		while(true) {
 			System.out.println("두 개의 정수를 입력하세요");
-			System.out.println("첫 번째 정수 >> ");
-			int num1 = scan.nextInt();
-			System.out.println("두 번째 정수 >> ");
-			int num2 = scan.nextInt();
+			System.out.println("종료를 원하시면 enter키를 입력하세요");
 			
-			System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
-			System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
-			System.out.printf("%d x %d = %d\n", num1, num2, num1 * num2);
-			System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
-			
-			System.out.println("종료를 원하시면 end를 입력하시고");
-			System.out.println("계속하길 원하시면 아무값이나 입력하세요");
-			String str = scan.next();
-			if(str.equals("end")) {
+			System.out.print("첫 번째 정수 >> ");
+			String num1 = scan.nextLine();
+			if(num1.length() < 1) {
 				break;
-			} else {
-				continue;
 			}
+			int intNum1 = Integer.parseInt(num1);
+			
+			System.out.print("두 번째 정수 >> ");
+			String num2 = scan.nextLine();
+			if(num2.length() < 1) {
+				break;
+			}
+			int intNum2 = Integer.parseInt(num2);
+			
+			System.out.printf("%d + %d = %d\n", intNum1, intNum2, intNum1 + intNum2);
+			System.out.printf("%d - %d = %d\n", intNum1, intNum2, intNum1 + intNum2);
+			System.out.printf("%d x %d = %d\n", intNum1, intNum2, intNum1 + intNum2);
+			System.out.printf("%d / %d = %d\n", intNum1, intNum2, intNum1 + intNum2);
+			
+			
 		}
 		System.out.println("종료합니다.");
 
