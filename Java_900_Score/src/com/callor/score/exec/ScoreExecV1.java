1350124880ff197c58ddcbce47961ab1b983f813
2gechan <7532602@gmail.com>
2023-05-02 10:59:25 +0900

    공용_변수,접근_제한자

---
+++ Java_900_Score/src/com/callor/score/exec/ScoreExecV1.java
@@ -0,0 +1,37 @@
+package com.callor.score.exec;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.ScoreServiceV1;
+
+public class ScoreExecV1 {
+
+	public static void main(String[] args) {
+
+		// 10명 학생의 점수를 저장할 객체 배열 선언
+		ScoreDto[] scoreList = new ScoreDto[10];
+
+		for (int i = 0; i < scoreList.length; i++) {
+			scoreList[i] = new ScoreDto();
+		}
+		
+		// ScoreService에 있는 method들을 사용하기 위한 준비
+		ScoreServiceV1 scService = new ScoreServiceV1();
+		
+		// ScoreExecV1 클래스에서 생성된(선언과 초기화가 완료된) 객체 배열을
+		// ScoreService 클래스의 scoreList 객체 배열변수에 전달하는 절차
+		scService.setScoreList(scoreList);
+		
+		// 점수 생성하기
+		scService.scoreRndMake();
+		
+		// 점수 출력하기
+		scService.scoreListPrint();
+		
+		// 과목의 총점 출력하기
+		scService.scoreTotalPrint();
+		
+		// 과목의 평균 출력하기
+		scService.scoreAvgPrint();
+	}
+
+}
