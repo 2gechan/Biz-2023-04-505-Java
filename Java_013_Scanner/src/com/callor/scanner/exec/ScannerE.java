f67cae3ad24ecd948841f67316c63b330c6ea218
2gechan <7532602@gmail.com>
2023-05-04 15:02:49 +0900

    랜덤수_찾기

--- Java_013_Scanner/src/com/callor/scanner/exec/ScannerE.java
+++ Java_013_Scanner/src/com/callor/scanner/exec/ScannerE.java
@@ -6,37 +6,39 @@ public class ScannerE {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
-		while(true) {
+
+		while (true) {
+			System.out.println("=".repeat(40));
 			System.out.println("GAME START");
+			System.out.println("=".repeat(40));
 			int rndNum = (int) (Math.random() * 10) + 1;
-			
-			while(true) {
-				
+
+			while (true) {
+
 				System.out.print("1 ~ 10 범위의 정수를 입력하세요 (종료 : Q) >> ");
-				
+
 				String strNum = scan.nextLine();
-				
-				if(strNum.equals("Q")) {
+
+				if (strNum.equals("Q")) {
 					System.out.println("GAME OVER");
 					return;
 				}
 				int num;
 				try {
 					num = Integer.valueOf(strNum);
-					if(rndNum == num) {
-						System.out.println("정답입니다");
+					if (rndNum == num) {
+						System.out.println("정답입니다.");
 						break;
-					} else if(rndNum > num) {
+					} else if (rndNum > num) {
 						System.out.println("랜덤 수 보다 작은 값을 입력하였습니다.");
-					} else {
+					} else if (rndNum < num) {
 						System.out.println("랜덤 수 보다 큰 값을 입력하였습니다.");
 					}
-					
+
 				} catch (Exception e) {
 					System.out.println("정수만 입력하세요");
 				}
-				
+
 			}
 		}
 	}
---
+++ Java_013_Scanner/src/com/callor/scanner/exec/ScannerG.java
@@ -0,0 +1,89 @@
+package com.callor.scanner.exec;
+
+import java.util.Scanner;
+
+public class ScannerG {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] nums = new int[6];
+		int[] nums2 = new int[6];
+
+		while (true) {
+
+			int rndNum = (int) (Math.random() * 45) + 1;
+
+			for (int i = 0; i < nums.length; i++) {
+				if (nums[i] > 0) { // nums[i]에 이미 값이 있을때
+					// 랜덤값과 nums[i]가 같으면 새로운 랜덤 수 생성
+					if (nums[i] == rndNum) {
+						break;
+					} else {
+						continue;
+					}
+				} else {
+					nums[i] = rndNum;
+					break;
+				}
+			}
+
+			if (nums[nums.length - 1] > 0) {
+				break;
+			}
+		}
+
+		int count = 0;
+		while (true) {
+
+			System.out.println("=".repeat(40));
+			System.out.println("GAME START");
+			System.out.println("=".repeat(40));
+			System.out.print("1 ~ 45 범위의 정수를 입력하세요 (종료 : Q) >> ");
+			String strNum = scan.nextLine();
+			if (strNum.equals("Q")) {
+				return;
+			}
+			try {
+				int num = Integer.valueOf(strNum);
+				if (num < 0 || num > 45) {
+					System.out.println("1 ~ 45 사이의 숫자를 입력하세요");
+				}
+
+				for (int index = 0; index < nums.length; index++) {
+					if (nums2[index] == num) {
+						System.out.println("이미 맞춘 값 입니다.");
+					} else if (nums[index] == num) {
+
+						nums2[index] = num;
+						count++;
+						System.out.println("정답입니다.");
+						if (count == nums.length) {
+							int tmp = 0;
+							for (int i = 0; i < nums.length; i++) {
+								for (int j = i + 1; j < nums.length; j++) {
+									if (nums[i] > nums[j]) {
+										tmp = nums[i];
+										nums[i] = nums[j];
+										nums[j] = tmp;
+									}
+								}
+							}
+							for(int i=0; i<nums.length; i++) {
+								System.out.print(nums[i] + " ");
+							}
+							return;
+						}
+
+						break;
+
+					}
+				}
+
+			} catch (Exception e) {
+				System.out.println("정수를 입력하세요");
+			}
+		}
+
+	}
+
+}
