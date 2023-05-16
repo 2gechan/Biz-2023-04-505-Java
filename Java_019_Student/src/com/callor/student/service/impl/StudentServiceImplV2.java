15572700dac23c5590a123e7b838811e3a1e6189
2gechan <7532602@gmail.com>
2023-05-16 15:09:33 +0900

    추가_수정

--- Java_019_Student/src/com/callor/student/service/impl/StudentServiceImplV2.java
+++ Java_019_Student/src/com/callor/student/service/impl/StudentServiceImplV2.java
@@ -26,14 +26,16 @@ public class StudentServiceImplV2 extends StudentServiceImplV1 {
 
 		StudentDto stDto = new StudentDto();
 		String stNum = "";
+		String stName = "";
+		String stDept = "";
+		int stGrade = 0;
+		String stTel = "";
 		while (true) {
 			System.out.print("학번 >> ");
 			stNum = scan.nextLine();
 
-			if (stNum.equals("QUIT")) {
+			if (stNum.equals("QUIT"))
 				break;
-			}
-			if (stNum.equals("QUIT")) break;
 			try {
 				int intNum = Integer.valueOf(stNum);
 				stNum = String.format("%04d", intNum);
@@ -45,57 +47,122 @@ public class StudentServiceImplV2 extends StudentServiceImplV1 {
 			break;
 
 		}
-		if(stNum.equals("QUIT")) return null;
-		
-		for(StudentDto dto : stdList) {
-			if(dto.stNum.equals(stNum)) {
+		if (stNum.equals("QUIT"))
+			return null;
+		int check = 0;
+		for (StudentDto dto : stdList) {
+			if (dto.stNum.equals(stNum)) {
 				System.out.println(Line.sLine(100));
 				System.out.println("동일학번 학생 데이터 있음, 데이터 수정!!");
 				System.out.println(dto.toString());
 				System.out.println(Line.sLine(100));
-				
+
 				// dto 의 어떤 데이터가 stDto 에 저장될까??
 				stDto = dto;
+				check++;
 			}
 		}
-		
-		System.out.print("이름 >> ");
-		String stName = scan.nextLine();
-		if (stName.equals("QUIT"))
-			return null;
+		if (check > 0) { // 이미 존재하는 데이터인 경우
 
-		System.out.print("학과 >> ");
-		String stDept = scan.nextLine();
-		if (stDept.equals("QUIT"))
-			return null;
-		int stGrade = 0;
-		String strGrade = "";
-		while (true) {
-
-			System.out.print("학년 >> ");
-			strGrade = scan.nextLine();
-			try {
-				if (strGrade.equals("QUIT"))
-					return null;
-				stGrade = Integer.valueOf(strGrade);
-			} catch (Exception e) {
-				System.out.println("학년은 정수를 입력하세요");
-				continue;
+			System.out.print("이름(" + stDto.stName + ") >> ");
+			stName = scan.nextLine();
+			if (stName.equals("QUIT"))
+				return null;
+			if (stName.equals("")) {
+				stName = stDto.stName;
 			}
-			if (stGrade < 1 || stGrade > 4) {
-				System.out.println("학년은 1 ~ 4 범위로 입력해주세요");
-			} else {
+
+			System.out.print("학과(" + stDto.stDept + ") >> ");
+			stDept = scan.nextLine();
+			if (stDept.equals("QUIT"))
+				return null;
+			if (stDept.equals("")) {
+				stDept = stDto.stDept;
+			}
+
+			while (true) {
+				System.out.print("학년(" + stDto.stGrade + ") >> ");
+				String strGrade = scan.nextLine();
+				if (strGrade.equals("QUIT"))
+					break;
+				try {
+					stGrade = Integer.valueOf(strGrade);
+				} catch (Exception e) {
+					// TODO: handle exception'
+					System.out.println("학년은 정수로 입력하세요");
+					continue;
+				}
+				if (stGrade < 1 || stGrade > 4) {
+					System.out.println("학년은 정수 1 ~ 4까지만 입력하세요");
+					continue;
+				}
 				break;
 			}
+
+			if (stGrade == 0)
+				return null;
+
+			System.out.print("전화번호(" + stDto.stTel + ") >> ");
+			stTel = scan.nextLine();
+			if (stTel.equals("QUIT"))
+				return null;
+			if (stTel.equals("")) {
+				stTel = stDto.stTel;
+			}
+		} else { // 신규 학생인 경우
+			System.out.print("이름(신규) >> ");
+			stName = scan.nextLine();
+			if (stName.equals("QUIT"))
+				return null;
+			if (stName.equals("")) {
+				stName = stDto.stName;
+			}
+
+			System.out.print("학과(신규) >> ");
+			stDept = scan.nextLine();
+			if (stDept.equals("QUIT"))
+				return null;
+			if (stDept.equals("")) {
+				stDept = stDto.stDept;
+			}
+
+			while (true) {
+				System.out.print("학년(0) >> ");
+				String strGrade = scan.nextLine();
+				if (strGrade.equals("QUIT"))
+					break;
+
+				try {
+					stGrade = Integer.valueOf(strGrade);
+				} catch (Exception e) {
+					// TODO: handle exception'
+					System.out.println("학년은 정수로 입력하세요");
+					continue;
+				}
+				if (stGrade < 1 || stGrade > 4) {
+					System.out.println("학년은 정수 1 ~ 4까지만 입력하세요");
+					continue;
+				}
+				break;
+			}
+
+			if (stGrade == 0)
+				return null;
+
+			System.out.print("전화번호(신규) >> ");
+			stTel = scan.nextLine();
+			if (stTel.equals("QUIT"))
+				return null;
+			if (stTel.equals("")) {
+				stTel = stDto.stTel;
+			}
 		}
 
-		System.out.print("전화번호 >> ");
-		String stTel = scan.nextLine();
-		if (stTel.equals("QUIT"))
-			return null;
-
-		stDto = new StudentDto(stNum, stName, stDept, stGrade, stTel);
-
+		stDto.stNum = stNum;
+		stDto.stName = stName;
+		stDto.stDept = stDept;
+		stDto.stGrade = stGrade;
+		stDto.stTel = stTel;
 		// stdList.add(stDto);
 		return stDto;
 
@@ -103,60 +170,9 @@ public class StudentServiceImplV2 extends StudentServiceImplV1 {
 
 	@Override
 	public void loadStudnet() {
-
-		while(true) {
-			StudentDto stDto = this.inputStudent();
-			if(stDto == null) break;
-			int index = 0 ;
-			for(index = 0 ; index < stdList.size() ; index++) {
-				// inputStudent() 가 return 한 dto 하고
-				// stdList 의 요소중에서 참조한 dto 하고
-				// 주소가 일치하냐?
-				// 주소가 일치하다 ==> stdList 에 있는 요소이다
-				if(stdList.get(index) == stDto) {
-					break;
-				}
-			}
-			if(index >= stdList.size()) {
-				stdList.add(stDto);
-			}
-			
-		}
-		
-		// 그리고 나머지는 내가 처리할께
-		System.out.println("여기는 V2");
-		OutputStream os = null;
-		PrintWriter out = null;
-		
-		try {
-			os = new FileOutputStream(Config.STUDENT_FILE);
-			out = new PrintWriter(os);
-			
-			for(StudentDto dto : stdList) {
-				out.printf("%s,",dto.stNum);
-				out.printf("%s,",dto.stName);
-				out.printf("%s,",dto.stDept);
-				out.printf("%d,",dto.stGrade);
-				out.printf("%s\n",dto.stTel);
-			}
-			out.close();
-			os.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void insertStudent() {
-
 		InputStream is = null;
 		Scanner scan = null;
-		
+
 		try {
 			is = new FileInputStream(Config.STUDENT_FILE);
 		} catch (FileNotFoundException e) {
@@ -166,10 +182,10 @@ public class StudentServiceImplV2 extends StudentServiceImplV1 {
 		scan = new Scanner(is);
 
 		int rows = 0;
-		while(scan.hasNext()) {
+		while (scan.hasNext()) {
 			String line = scan.nextLine();
 			String[] student = line.split(",");
-			rows ++;
+			rows++;
 			try {
 				StudentDto stDto = new StudentDto();
 				stDto.stNum = student[Index.STUDENT.ST_NUM];
@@ -185,6 +201,58 @@ public class StudentServiceImplV2 extends StudentServiceImplV1 {
 		}
 		scan.close();
 		System.out.println("Load 한 데이터 개수 : " + stdList.size());
+
+	}
+
+	@Override
+	public void insertStudent() {
+		while (true) {
+			StudentDto stDto = this.inputStudent();
+			if (stDto == null)
+				break;
+			int index = 0;
+			for (index = 0; index < stdList.size(); index++) {
+				// inputStudent() 가 return 한 dto 하고
+				// stdList 의 요소중에서 참조한 dto 하고
+				// 주소가 일치하냐?
+				// 주소가 일치하다 ==> stdList 에 있는 요소이다
+				if (stdList.get(index) == stDto) {
+					break;
+				}
+			}
+			if (index >= stdList.size()) {
+				stdList.add(stDto);
+			}
+
+		}
+
+		// 그리고 나머지는 내가 처리할께
+		System.out.println("여기는 V2");
+		OutputStream os = null;
+		PrintWriter out = null;
+
+		try {
+			os = new FileOutputStream(Config.STUDENT_FILE);
+			out = new PrintWriter(os);
+
+			for (StudentDto dto : stdList) {
+				out.printf("%s,", dto.stNum);
+				out.printf("%s,", dto.stName);
+				out.printf("%s,", dto.stDept);
+				out.printf("%d,", dto.stGrade);
+				out.printf("%s\n", dto.stTel);
+			}
+			out.close();
+			os.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
